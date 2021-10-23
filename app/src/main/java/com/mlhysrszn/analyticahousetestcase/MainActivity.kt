8bc1da4f8c4c0aab6c15d1daed2f67fe63eef4d7
Melih Yaşar SÖZEN <mlhysrszn@gmail.com>
2021-10-23 22:07:56 +0300

    dependencies added

--- app/src/main/java/com/mlhysrszn/analyticahousetestcase/MainActivity.kt
+++ app/src/main/java/com/mlhysrszn/analyticahousetestcase/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.mlhysrszn.analyticahousetestcase
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.mlhysrszn.analyticahousetestcase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
